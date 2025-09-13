3bd6f7901c7e55f7a28de6c6edc1ebf29df70000
ifaakash <aakashc.appregis@gmail.com>
2025-09-14 00:26:15 +0530

    refactor(terraform): Add indentation in `variables.tf`

--- sandbox/variables.tf
+++ sandbox/variables.tf
@@ -1,26 +1,26 @@
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
-  type= string
-  default= "10.0.0.0/22"
+  type    = string
+  default = "10.0.0.0/22"
 }
 
 variable "prefix" {
-  type= string
+  type    = string
   default = "otcomes-sandbox"
 }
 
 variable "public_subnet_cidr_block" {
-  type = string
+  type    = string
   default = "10.0.0.0/24"
 }
 
 variable "private_subnet_cidr_block" {
-  type = string
-  default= "10.0.0.0/28"
+  type    = string
+  default = "10.0.0.0/28"
 }
 
 variable "default_tags" {
