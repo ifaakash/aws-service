33bce8b86b7484bc0ddeaafbcc137d78f705c753
ifaakash <aakashc.appregis@gmail.com>
2025-09-14 04:59:38 +0530

    chore(terraform): Add `force_destroy` for S3 bucket

--- sandbox/state_management/main.tf
+++ sandbox/state_management/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
-
-  tags = merge({ "Name" = "${var.prefix}-terraform-state" }, var.default_tags)
+  bucket        = var.bucket_name
+  force_destroy = true
+  tags          = merge({ "Name" = "${var.prefix}-terraform-state" }, var.default_tags)
 }
