193596df2ae5b47da40d339fd61eb6e49b8e341b
ifaakash <aakashc.appregis@gmail.com>
2025-09-15 01:53:50 +0530

    chore(networking): Allow outbound traffic to internet from instance
    - Allow internal traffic from instance for downloading packages

--- sandbox/networking/security-group.tf
+++ sandbox/networking/security-group.tf
@@ -38,6 +38,6 @@ resource "aws_vpc_security_group_egress_rule" "egress_all" {
   security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "tcp"
-  from_port         = 0
-  to_port           = 0
+  from_port         = -1
+  to_port           = -1
 }
