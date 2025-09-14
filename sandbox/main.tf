242971c34a207c2f0f27266090a8d0b0e847e523
ifaakash <aakashc.appregis@gmail.com>
2025-09-14 16:55:17 +0530

    refactor(terraform): Deploy instance and add backend config

--- sandbox/main.tf
+++ sandbox/main.tf
@@ -7,49 +7,49 @@ terraform {
   }
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "otcomes-sandbox-terraform-state"
-#     key    = "terraform-state-file"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "otcomes-sandbox-terraform-state"
+    key    = "terraform-state-file"
+    region = "us-east-1"
+  }
+}
 
 
 provider "aws" {
   region = var.aws_region
 }
 
-# module "network" {
-#   source                    = "./networking"
-#   prefix                    = var.prefix
-#   vpc_cidr_block            = var.vpc_cidr_block
-#   public_subnet_cidr_block  = var.public_subnet_cidr_block
-#   private_subnet_cidr_block = var.private_subnet_cidr_block
-#   ssh_access_cidr           = var.ssh_access_cidr
-#   default_tags              = var.default_tags
-# }
-
-
-# module "instance" {
-#   source               = "./instance"
-#   prefix               = var.prefix
-#   ami_id               = var.ami_id
-#   network_interface_id = module.network.network_interface_id
-#   # associate_public_ip_address = var.associate_public_ip_address
-#   interface_id  = module.network.network_interface_id
-#   instance_type = var.instance_type
-#   default_tags  = var.default_tags
-
-#   depends_on = [
-#     module.network
-#   ]
-# }
-
-module "state" {
-  source = "./state_management"
+module "network" {
+  source                    = "./networking"
+  prefix                    = var.prefix
+  vpc_cidr_block            = var.vpc_cidr_block
+  public_subnet_cidr_block  = var.public_subnet_cidr_block
+  private_subnet_cidr_block = var.private_subnet_cidr_block
+  ssh_access_cidr           = var.ssh_access_cidr
+  default_tags              = var.default_tags
 }
 
-output "s3_bucket_name" {
-  value = module.state.s3_bucket_name
+
+module "instance" {
+  source               = "./instance"
+  prefix               = var.prefix
+  ami_id               = var.ami_id
+  network_interface_id = module.network.network_interface_id
+  # associate_public_ip_address = var.associate_public_ip_address
+  interface_id  = module.network.network_interface_id
+  instance_type = var.instance_type
+  default_tags  = var.default_tags
+
+  depends_on = [
+    module.network
+  ]
 }
+
+# module "state" {
+#   source = "./state_management"
+# }
+
+# output "s3_bucket_name" {
+#   value = module.state.s3_bucket_name
+# }
